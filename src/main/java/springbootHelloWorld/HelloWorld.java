6329d8e0231a2bc2927432bfdd93371ede459c62
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-25 11:58:10 -0500

    Update HelloWorld.java

--- src/main/java/springbootHelloWorld/HelloWorld.java
+++ src/main/java/springbootHelloWorld/HelloWorld.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-
+#Adding comment to coomit
 @Controller
 @EnableAutoConfiguration
 public class HelloWorld{
